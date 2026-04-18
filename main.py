8d27eef08db174a8035942a230c9a5d0e04d9a85
elianarias-phyton <ariaselian2002@gmail.com>
2026-04-18 14:18:41 -0300

    Ejercicio

---
+++ main.py
@@ -0,0 +1,209 @@
+#EJERCICIO 1
+
+# print ("Hola mundo")
+
+#EJERCICIO 2
+
+# nombre = input("Escriba su nombre y será saludado: ")
+
+# print ("Hola " , nombre)
+
+#EJERCICIO 3
+
+# nombre = input("Escriba su nombre: ")
+# apellido = input("Escriba su apellido: ")
+
+# print ("¡Bienvenido/a " , nombre , apellido ,"!")
+
+#EJERCICIO 4
+
+# numero = input("Escriba su numero: ")
+
+# print ("Usted ha ingresado el número " , numero)
+
+#EJERCICIO 5
+
+# numero_1 = int(input("Escriba su primer numero: "))
+# numero_2 = int(input("Escriba su segundo numero: "))
+
+# suma = numero_1 + numero_2
+
+# print("La suma de sus numeros es: " , suma)
+
+# EJERCICIO 6
+
+# nombre = input("Escriba su nombre: ")
+
+# print ("Hola " , nombre)
+
+# numero_1 = int(input("Escriba su primer numero: "))
+# print("Su primer número es " , numero_1)
+
+# numero_2 = int(input("Escriba su segundo numero: "))
+# print("Su segundo número es " , numero_2)
+
+# numero_3 = int(input("Escriba su tercer numero: "))
+# print("Su tercer número es " , numero_3)
+
+# producto = numero_1 * numero_2 * numero_3
+
+# print("Hola " , nombre, "El producto de los 3 números que ingresaste es " , producto)
+
+#EJERCICIO 7
+
+# numero_1 = int(input("Escriba su primer numero: "))
+# print("Su primer número es " , numero_1)
+#
+# numero_2 = int(input("Escriba su segundo numero: "))
+# print("Su segundo número es " , numero_2)
+#
+# numero_3 = int(input("Escriba su tercer numero: "))
+# print("Su tercer número es " , numero_3)
+#
+# resultado = (numero_1 + numero_2) - numero_3
+#
+# print("El resultado es " , resultado)
+
+#EJERCICIO 8
+
+# numero_1 = int(input("Escriba su primer numero: "))
+# print("Su primer número es " , numero_1)
+#
+# numero_2 = int(input("Escriba su segundo numero: "))
+# print("Su segundo número es " , numero_2)
+#
+# numero_3 = int(input("Escriba su tercer numero: "))
+# print("Su tercer número es " , numero_3)
+#
+# numero_4 = int(input("Escriba su cuarto numero: "))
+# print("Su cuarto número es " , numero_4)
+#
+# resultado = (numero_1 + numero_2) * (numero_3 - numero_4)
+#
+# print("Su resultado es " , resultado)
+
+#EJERCICIO 9
+
+# numero_1 = int(input("Escriba su primer numero: "))
+# print("Su primer número es " , numero_1)
+
+# numero_2 = int(input("Escriba su segundo numero: "))
+# print("Su segundo número es " , numero_2)
+
+# resultado = numero_1 % numero_2
+
+# print("El resultado es " , resultado)
+
+#EJERCICIO 10
+
+# base_1 = int(input("Ingresa la base del triangulo "))
+# print ("Tu base es ", base_1) #BASE
+
+# altura_2 = int(input("Ingresa la altura del triangulo "))
+# print ("Tu altura es ", altura_2) #ALTURA
+
+# área = (base_1 * altura_2) / 2
+
+# print("El área de tu triángulo es: " , área)
+
+#EJERCICIO 11
+
+# lado_1 = int(input("Ingresa la primer medida del triangulo "))
+# print ("Tu medida es ", lado_1)
+
+# lado_2 = int(input("Ingresa la segunda medida del triangulo "))
+# print ("Tu medida es ", lado_2)
+
+# lado_3 = int(input("Ingresa la tercer medida del triangulo "))
+# print ("Tu medida es ", lado_3)
+
+# suma = lado_1 + lado_2 + lado_3
+
+# print("El perimetro de tu triangulo es ", suma)
+
+#EJERCICIO 12
+
+# base = int (input("Defini la base de tu triángulo "))
+# print ("La base de tu triangulo es ", base)
+
+# altura = int (input("Defini la altura de tu triángulo "))
+# print ("La altura de tu triangulo es ", altura)
+
+# área = (base * altura) / 2
+
+# print ("El área de ti triángulo es ", área)
+
+#EJERCICIO 13
+
+# ángulo_1 = int(input("Inserte el primer ángulo de su triangulo "))
+# print ("Su primer ángulo mide " , ángulo_1)
+
+# ángulo_2 = int(input("Inserte el segundo ángulo de su triangulo "))
+# print ("Su segundo ángulo mide " , ángulo_2)
+
+# ángulo_restante = 180 - (ángulo_1 + ángulo_2)
+# print ("Su tercer ángulo mide " , ángulo_restante)
+
+#EJERCICIO 14
+
+# Elian Arias - 19/11/2025
+
+# horas = int (input ("Introduzca su horario en el siguiente formato,"\
+#                     " horas, minutos y segundos. El programa lo convertira en segundos. Coloque su hora: "))
+# print ("La hora es " , horas)
+#
+# minutos = int (input ("Coloque sus minutos: "))
+# print ("Los minutos son " , minutos)
+#
+# segundos = int (input ("Coloque sus segundos: "))
+# print ("Los segundos son " , segundos)
+#
+# print ("Tu horario es " ,":", horas ,":", minutos ,":", segundos)
+#
+# convertido_a_segundos = (horas * 60 * 60) + (minutos * 60) + segundos
+#
+# print ("Tu horario convertido a segundos es equivalente a: " , convertido_a_segundos)
+
+#EJERCICIO 15
+
+# número = int (input("Ingresa un número y te voy a mostrar la tabla de multiplicar del mismo: "))
+
+# resultado_1 = número * 1
+# print (número , "x 1" , "=" , resultado_1)
+
+# resultado_2 = número * 2
+# print (número , "x 2" , "=" , resultado_2)
+
+# resultado_3 = número * 3
+# print (número , "x 3" , "=" , resultado_3)
+
+# resultado_4 = número * 4
+# print (número , "x 4" , "=" , resultado_4)
+
+# resultado_5 = número * 5
+# print (número , "x 5" , "=" , resultado_5)
+
+# resultado_6 = número * 6
+# print (número , "x 6" , "=" , resultado_6)
+
+# resultado_7 = número * 7
+# print (número , "x 7" , "=" , resultado_7)
+
+# resultado_8 = número * 8
+# print (número , "x 8" , "=" , resultado_8)
+
+# resultado_9 = número * 9
+# print (número , "x 9" , "=" , resultado_9)
+
+# resultado_10 = número * 10
+# print (número , "x 10" , "=" , resultado_10)
+
+#EJERCICIO 16
+
+# horas_trabajadas = int (input("Ingrese cuantas horas trabajo: "))
+
+# valor_hora = int (input("Ingrese cual es el valor de su hora: "))
+
+# sueldo = horas_trabajadas * valor_hora
+
+# print("Tu sueldo este mes es de: $" , sueldo)
